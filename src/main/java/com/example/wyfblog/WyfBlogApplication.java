d3b3f00817ba7c715d733bfca95df3f0d69e6948
WenYaFei <1506825038@qq.com>
2024-05-30 16:39:04 +0800

    Initial commit

---
+++ src/main/java/com/example/wyfblog/WyfBlogApplication.java
@@ -0,0 +1,13 @@
+package com.example.wyfblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WyfBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WyfBlogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/wyfblog/WyfBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.wyfblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WyfBlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
